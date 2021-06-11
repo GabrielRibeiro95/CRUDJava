a5177b98c910a8b97f9531cfd4741630436aa172
GabrielRibeiro95 <gabriel_alvesribeiro@hotmail.com>
2021-06-10 23:23:44 -0300

    Update Conexao.java

--- Conexao.java
+++ Conexao.java
@@ -4,12 +4,15 @@ import java.sql.SQLException;
 
 public class Conexao {
 	
+	//Defina aqui as credenciais do banco
+	static final String SERVER="localhost", PORT="3306", USER="root", PW="";	
+	
 	public Connection getConexao() {
 		Connection c=null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Conectando ao banco");	
-			c=DriverManager.getConnection("jdbc:mysql://localhost:3306/academico","root","");
+			c=DriverManager.getConnection("jdbc:mysql://"+SERVER+":"+PORT+"/academico", USER, PW);
 			System.out.println("Conectado ao banco");
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
